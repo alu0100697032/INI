6637c9ec7f3c9f0c01449112998e2af4a1193361
Jorge Gómez Weyler <alu0100717723@ull.edu.es>
2014-02-19 22:03:02 +0000

    Métodos implementados.

--- ini.js
+++ ini.js
@@ -27,3 +27,55 @@ function calculate(evt) {
 }
 
 var temp = '<li> <span class = "<%= token.type %>"> <%= match %> </span>\n';
+
+function tokensToString(tokens) {
+   var r = '';
+   for(var i=0; i < tokens.length; i++) {
+     var t = tokens[i]
+     var s = JSON.stringify(t, undefined, 2);
+     s = _.template(temp, {token: t, match: s});
+     r += s;
+   }
+   return '<ol>\n'+r+'</ol>';
+}
+
+function lexer(input) {
+  var blanks         = /^\s+/;                        //Identifica espacios
+  var iniheader      = /^\[([^\]\r\n]+)\]/;           //Identifica campos de cabecera "[cabecera]""
+  var comments       = /^[;#](.*)/;                   //Identifica comentarios
+  var nameEqualValue = /^([^=;\r\n]+)=([^;\r\n]*)/;   //Identifica valores.
+  var any            = /^(.|\n)+/;                    // Un . o retorno de carro.
+
+  var out = [];   //Irá almacenando las cadenas de salida aquí.
+  var m = null;   //Almacenará de forma temporal la cadena leída, para que después introducirla en el vector out.
+                  // Entrará en el if correspondiente según "el tipo" de campo que haya leído.
+
+  while (input != '') {
+    if (m = blanks.exec(input)) {
+      input = input.substr(m.index+m[0].length);
+      out.push({ type : 'blanks', match: m });
+    }
+    else if (m = iniheader.exec(input)) {
+      input = input.substr(m.index+m[0].length);
+      out.push({ type: 'header', match: m });
+    }
+    else if (m = comments.exec(input)) {
+      input = input.substr(m.index+m[0].length);
+      out.push({ type: 'comments', match: m });
+    }
+    else if (m = nameEqualValue.exec(input)) {
+      /* while (match casa con /\\$/) concatena la siguiente línea */
+      input = input.substr(m.index+m[0].length);
+      out.push({ type: 'nameEqualValue', match: m });
+    }
+    else if (m = any.exec(input)) {
+      out.push({ type: 'error', match: m });
+      input = '';
+    }
+    else {
+      alert("Fatal Error!"+substr(input,0,20));
+      input = '';
+    }
+  }
+  return out;
+}
