1c738618766d795864a5584c4327f1143564c016
Victor Hernandez Perez <alu0100697032@ull.edu.es>
2014-02-19 22:55:06 +0000

    Añadidas dos pruebas más

--- test/tests.js
+++ test/tests.js
@@ -6,4 +6,12 @@ suite('Conjunto de pruebas INI', function() {
         var tokens = lexer('[hola]');
 		assert.equal(tokens[0].type,'header');
     });
+    test('¿El token es "nombre = valor"?', function() {
+        var tokens = lexer('nombre = valor');
+		assert.equal(tokens[0].type,'nameEqualValue');
+    });
+    test('¿El token es un comentario?', function() {
+        var tokens = lexer(';Esto es un comentario');
+		assert.equal(tokens[0].type,'comments');
+    });
 });
